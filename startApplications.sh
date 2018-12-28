4bb90033b5543448858e7ac623fed22e7d4af946
Davey Kropf <dkropf@kaliber.net>
2018-12-28 16:23:28 +0100

    Added startup script

---
+++ startApplications.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BIN_ROOT="/usr/bin"
+APPLICATION_ROOT="/home/pi/kaliber-billy-bassistant"
+
+# Start application
+echo "Starting startup_motors.py"
+$BIN_ROOT/python3 $APPLICATION_ROOT/startup_motors.py
+
+echo "Starting Mouth.py"
+$BIN_ROOT/python3 $APPLICATION_ROOT/mouth.py &
+
+echo "Starting Google Assistant"
+$BIN_ROOT/python3 -m hotword --project-id billybassistant --device-model-id billybassistant-billybassistant2-ef1kwz
--- startup_motors.py
+++ startup_motors.py
@@ -3,18 +3,28 @@ import time
 from adafruit_motorkit import MotorKit
 
 kit = MotorKit()
-COUNT = 5
+COUNT = 10
 CURRENT_COUNT = 0
 
 while CURRENT_COUNT < COUNT:
+  kit.motor2.throttle = 1
+  time.sleep(0.1)
+  kit.motor2.throttle = 0
+  time.sleep(0.1)
+  kit.motor2.throttle = -1
+  time.sleep(0.1)
+  kit.motor2.throttle = 0
+  time.sleep(0.1)
+
   kit.motor1.throttle = -1
-  time.sleep(0.2)
+  time.sleep(0.1)
   kit.motor1.throttle = 0
-  time.sleep(0.2)
+  time.sleep(0.1)
   kit.motor1.throttle = 1
-  time.sleep(0.2)
+  time.sleep(0.1)
   kit.motor1.throttle = 0
-  time.sleep(0.2)
+  time.sleep(0.1)
   CURRENT_COUNT += 1
 
+kit.motor2.throttle = 0
 kit.motor1.throttle = -1
